ab7cb20cadbd6160a8e8860dc7e1636e6ada5ba4
Colin Davidson <colrdavidson@gmail.com>
2019-04-06 02:45:09 -0400

    Add very basic/incomplete support for function-ish and expression-ish parsing

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define fatal(...) do { printf(__VA_ARGS__); exit(1); } while(0);
+#define fatal(...) do { printf("\033[0;31m"); printf(__VA_ARGS__); printf("\033[0m"); exit(1); } while(0);
 
 void *emalloc(size_t sz) {
     void *ptr = malloc(sz);
@@ -27,8 +27,20 @@ int is_digit(char c) {
     return 0;
 }
 
+int strtoi(char *s, int sz) {
+    int ret = 0;
+    int shift = 1;
+    for (int i = sz - 1; i >= 0; i--) {
+        ret += (s[i] - '0') * shift;
+        shift *= 10;
+    }
+
+    return ret;
+}
+
 char *get_next_token(char *buf, char *end, int *size) {
-    char *token;
+    char *token = NULL;
+    *size = 0;
 
     while (buf < end) {
         if (is_digit(*buf)) {
@@ -36,31 +48,34 @@ char *get_next_token(char *buf, char *end, int *size) {
             while (is_digit(*token)) {
                 token++;
             }
-            int token_size = token - buf;
 
-            *size = token_size;
-            return buf;
+            *size = token - buf;
+            token = buf;
+            goto exit;
         } else if (is_char(*buf)) {
-            char *token = buf;
+            token = buf;
             while (is_char(*token)) {
                 token++;
             }
-            int token_size = token - buf;
 
-            *size = token_size;
-            return buf;
+            *size = token - buf;
+            token = buf;
+            goto exit;
         } else if (*buf == ';' || *buf == '(' || 
                    *buf == ')' || *buf == '{' || *buf == '}') {
-
+            token = buf;
             *size = 1;
-            return buf;
+            goto exit;
         }
 
         buf++;
     }
     
-    *size = 0;
-    return NULL;
+exit:
+//    if (*size)
+//        printf("token: %.*s\n", *size, token); 
+
+    return token;
 }
 
 int main() {
@@ -82,31 +97,59 @@ int main() {
     if (n != file_size)
         fatal("Failed to read the whole file! %zu != %zu\n", n, file_size);
 
-    int line_num = 1;
-    int line_off = 0;
-
     int size;
     char *file_ptr = file_buf;
     char *file_end = file_buf + file_size;
     while (file_ptr < file_end) {
-        line_off++;
-        if (*file_ptr == '\n') {
-            line_num++;
-            line_off = 1;
-        }
 
         char *token = get_next_token(file_ptr, file_end, &size);
         if (!size) break;
 
-        printf("token: %.*s\n", size, token);
-        file_ptr = token + size;
-
-        if (!memcmp(token, "return", size)) {
-            printf("Found keyword return!\n");
-        }
-
         if (!memcmp(token, "int", size)) {
-            printf("Found token int!\n");
+            file_ptr = token + size;
+            token = get_next_token(file_ptr, file_end, &size);
+            if (!size) fatal("Expected function name!\n");
+
+            char *func_name = token;
+            int func_name_sz = size;
+            printf("found function %.*s\n", func_name_sz, func_name);
+
+            file_ptr = token + size;
+            token = get_next_token(file_ptr, file_end, &size);
+            if (!size || *token != '(')
+                fatal("Expected (\n");
+
+            file_ptr = token + size;
+            while (file_ptr < file_end) {
+                token = get_next_token(file_ptr, file_end, &size);
+                if (!size) fatal("Expected )\n");
+                if (*token == ')') break;
+                file_ptr = token + size;
+            }
+
+            file_ptr = token + size;
+            token = get_next_token(file_ptr, file_end, &size);
+            if (!size || *token != '{')
+                fatal("Expected {\n");
+
+            // Function Body
+            file_ptr = token + size;
+            while (file_ptr < file_end) {
+                token = get_next_token(file_ptr, file_end, &size);
+                if (!size) fatal("Expected }\n");
+                if (*token == '}') break;
+                file_ptr = token + size;
+
+                if (!memcmp(token, "return", size)) {
+                    token = get_next_token(file_ptr, file_end, &size);
+                    if (!size) fatal("Expected constant!\n");
+                    file_ptr = token + size;
+
+                    printf("returning %d\n", strtoi(token, size));
+                }
+            }
         }
+
+        file_ptr = token + size;
     }
 }
